92681c247b5e57b271168a7cb09dc24c86c2d485
Haorsis <cadetgauthier3@gmail.com>
2022-10-12 16:21:45 +0200

    refactor player test

--- SnakeGameTests/entity/PlayerTest.cs
+++ SnakeGameTests/entity/PlayerTest.cs
@@ -1,34 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SnakeGame.entity {
+﻿namespace SnakeGame.entity {
     [TestClass]
     public class PlayerTest {
+
+        public Player player { get; set; }
+        public PlayerTest() {
+            player = new Player("Martin");
+            player.playerCase = 50;
+        }
+
         [TestMethod]
         public void ThrowDice_CheckOvercome() {
-            Player player = new Player("Martin");
-            player.playerCase = 50;
-
             player.ThrowDice(50, 25);
-
             Assert.AreEqual(25, player.playerCase);
         }
 
         [TestMethod]
         public void CheckPlayerPosition_Win() {
-            Player player = new Player("Martin");
-            player.playerCase = 50;
-
             Assert.IsTrue(player.CheckPlayerPosition(50, 25));
         }
 
         [TestMethod]
         public void CheckPlayerPosition_OvercomeOrNothing() {
-            Player player = new Player("Martin");
-
             player.playerCase = 10;
             Assert.IsFalse(player.CheckPlayerPosition(50, 25));
 
